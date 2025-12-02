52f8c88ab0d0b5e91b85fc38b506b64c358accf9
Gogul Krishnan <167337046+Gogul2005@users.noreply.github.com>
2025-12-02 14:08:28 +0530

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Transaction
+{
+    static void Main(string[] args)
+    {
+        string transactionID = "TXN1234ABCD9";
+        decimal amount = 1000000.75m;
+        bool isInternational = true;
+        decimal customerRating = 4.75m;
+        DateTime transactionTimestamp = DateTime.Now;
+        int rewardPoints = 2500000;
+
+        Console.WriteLine($"ID: {transactionID}");
+        Console.WriteLine($"Amount: ₹{amount}");
+        Console.WriteLine($"International: {isInternational}");
+        Console.WriteLine($"Rating: {customerRating}");
+        Console.WriteLine($"Timestamp: {transactionTimestamp}");
+        Console.WriteLine($"Reward Points: {rewardPoints}");
+    }
+}
